f061e3af7da29a6102ec2e19f679389e54663798
FEDEFED <47941652+FEDEFED@users.noreply.github.com>
2024-01-04 14:11:27 +0100

    Update config.js

--- config.js
+++ config.js
@@ -83,9 +83,9 @@ const CONFIG = {
 		},
 		{
 			id: '6',
-			name: 'Odysee',
-			icon: 'youtube',
-			link: 'https://odysee.com/',
+			name: 'Music',
+			icon: 'headphones',
+			link: 'https://music.youtube.com',
 		},
 	],
 
